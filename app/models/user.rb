af758a954405a4a13262e3709cd1e069b98dea4d
Federico Sapuppo <fedesapuppo@hotmail.com>
2023-05-01 12:56:32 +0300

    define 'reports' method in 'User' model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          has_many :friends
+
+         def reports
+          Report.where(user_id: self.id)
+        end
 end
